8a87bb4dc8df3776111f2e6b2017423df579977e
myusername <myemil@gmail.com>
2023-03-17 03:03:53 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main . A program that print a line using the printf fuction
+ * main - A program that print a line using the printf fuction
  * Return: 0 (Success)
  */
 int main(void)
